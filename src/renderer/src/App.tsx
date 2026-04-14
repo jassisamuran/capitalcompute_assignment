1077197a5112953565a3dff8595af9b3c018606b
jassisamuran <samuran3132@gmail.com>
2026-04-15 04:06:03 +0530

    added main component for showing frontend

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+import { SourceGrid } from "./components/SourceGrid";
+import { Source } from "./types";
+
 export default function App() {
-  return <div style={{ color: 'white' }}>one is one</div>
+  const [sources, setSources] = useState<Source[]>([]);
+  const loadSources = async () => {
+    try {
+      const src = await window.electronAPI.getSources();
+      console.log("now", src);
+      setSources(src)
+    } catch (err) {
+      console.log("get-sources failed:", err);
+    }
+  };
+  // loadSources();
+  return <div style={{ color: "blue" }}>one issd one
+ 
+        <div
+        className="h-8 flex items-center "
+      >
+        <span className="text-[20px] font-mono">
+          CAPTURA
+        </span>
+      </div>
+
+      <main>
+<SourceGrid
+sources={sources}
+onLoad={()=>console.log('onload')}
+loading={true}
+onSelect={()=>console.log('onload')}/>
+
+      </main>
+
+  
+  </div>;
 }
